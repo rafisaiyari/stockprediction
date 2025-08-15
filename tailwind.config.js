31035f81b21862ab161447e03e116a3a6c39facf
rafisaiyari <rafisaiyari@ymail.com>
2025-08-15 20:54:25 +0800

    Add all local files with new folder structure

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// filepath: c:\Users\Legion 5 Pro\Downloads\stocks\stock-prediction-app\tailwind.config.js
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme: {
+      extend: {},
+  },
+  plugins: [],
+};
